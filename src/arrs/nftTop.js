acbb6f0007e4ec32b3510f83e201787f77269a59
stillistica <rozhkovavika24@gmail.com>
2025-09-11 20:36:20 +0300

    add nftTop arr

---
+++ src/arrs/nftTop.js
@@ -0,0 +1,56 @@
+import imgOneDeskOne from '/img/top-nft/card-one/desk-1x.webp';
+import imgOneDeskTwo from '/img/top-nft/card-one/desk-2x.webp';
+import imgOneMobOne from '/img/top-nft/card-one/mob-1x.webp';
+import imgOneMobTwo from '/img/top-nft/card-one/mob-2x.webp';
+import avatarOne from '/img/top-nft/card-one/avatar.webp';
+
+// import imgOneDeskOne from '/img/top-nft/card-one/desk-1x.webp';
+// import imgOneDeskTwo from '/img/top-nft/card-one/desk-2x.webp';
+// import imgOneMobOne from '/img/top-nft/card-one/mob-1x.webp';
+// import imgOneMobTwo from '/img/top-nft/card-one/mob-2x.webp';
+// import avatarOne from '/img/top-nft/card-one/avatar.webp';
+
+export const nftTop = [
+  {
+    imgDeskOne: imgOneDeskOne,
+    imgDeskTwo: imgOneDeskTwo,
+    imgMobOne: imgOneMobOne,
+    imgMobTwo: imgOneMobTwo,
+    title: 'Apollo Eth #1289 NFT',
+    avatarUrl: avatarOne,
+    accName: '@jhonartist',
+    eth: 'ETH 9.31',
+    usd: '$28,8846',
+    hours: '18',
+    mins: '25',
+    secs: '44',
+  },
+  // {
+  //   imgDeskOne: imgOneDeskOne,
+  //   imgDeskTwo: imgOneDeskTwo,
+  //   imgMobOne: imgOneMobOne,
+  //   imgMobTwo: imgOneMobTwo,
+  //   title: 'Apollo Eth #1289 NFT',
+  //   avatarUrl: avatarOne,
+  //   accName: '@jhonartist',
+  //   eth: 'ETH 9.31',
+  //   usd: '$28,8846',
+  //   hours: '18',
+  //   mins: '25',
+  //   secs: '44',
+  // },
+  // {
+  //   imgDeskOne: imgOneDeskOne,
+  //   imgDeskTwo: imgOneDeskTwo,
+  //   imgMobOne: imgOneMobOne,
+  //   imgMobTwo: imgOneMobTwo,
+  //   title: 'Apollo Eth #1289 NFT',
+  //   avatarUrl: avatarOne,
+  //   accName: '@jhonartist',
+  //   eth: 'ETH 9.31',
+  //   usd: '$28,8846',
+  //   hours: '18',
+  //   mins: '25',
+  //   secs: '44',
+  // },
+];
