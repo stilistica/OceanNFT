6e264395f332e4d7ffaf02b425699d07cb6e9a35
stillistica <rozhkovavika24@gmail.com>
2025-09-14 12:11:23 +0300

    update photo

--- src/arrs/collections.js
+++ src/arrs/collections.js
@@ -1,20 +1,11 @@
-// import imgDeskOne from '/img/collections/bagr-desk-1x.webp';
-// import imgDeskTwo from '/img/collections/bagr-desk-2x.webp';
-// import imgMobOne from '/img/collections/bagr-mob-1x.webp';
-// import imgMobTwo from '/img/collections/bagr-mob-2x.webp';
+import imgDeskOne from '/img/collections/bagr-desk-1x.webp';
+import imgDeskTwo from '/img/collections/bagr-desk-2x.webp';
+import imgMobOne from '/img/collections/bagr-mob-1x.webp';
+import imgMobTwo from '/img/collections/bagr-mob-2x.webp';
 
-// import avatarOne from '/img/collections/card-one.webp';
-// import avatarTwo from '/img/collections/card-two.webp';
-// import avatarThree from '/img/collections/card-three.webp';
-
-import imgDeskOne from '/img/collections/bagr-desk-1x.png';
-import imgDeskTwo from '/img/collections/bagr-desk-2x.png';
-import imgMobOne from '/img/collections/bagr-mob-1x.png';
-import imgMobTwo from '/img/collections/bagr-mob-2x.png';
-
-import avatarOne from '/img/collections/card-one.png';
-import avatarTwo from '/img/collections/card-two.png';
-import avatarThree from '/img/collections/card-three.png';
+import avatarOne from '/img/collections/card-one.webp';
+import avatarTwo from '/img/collections/card-two.webp';
+import avatarThree from '/img/collections/card-three.webp';
 
 export const collections = [
   {
